0eb66a1d81ef70acd7994408223e02b88899868d
kevinrupert <kevinrupert.design@gmail.com>
2015-12-29 14:45:11 -0500

    Season 2.

--- js/main.js
+++ js/main.js
@@ -1,75 +1,133 @@
-var $logo = $('.logo');
-var $s1e2bg = $('.s1-e2-bg');
-var $s1e3bg = $('.s1-e3-bg');
-var $s1e4bg = $('.s1-e4-bg');
-var $s1e5bg = $('.s1-e5-bg');
-var $s1e6bg = $('.s1-e6-bg');
-var $s1e7bg = $('.s1-e7-bg');
-var $s1e8bg = $('.s1-e8-bg');
-var $s1e9bg = $('.s1-e9-bg');
-var $s1e10bg = $('.s1-e10-bg');
-
-
-$s1e2bg.waypoint(function (direction) {
+$('.s1-e2-bg').waypoint(function (direction) {
     if (direction == 'down') {
-        $logo.addClass('s1-e2-type');
+        $('.logo').addClass('s1-e2-type');
     } else {
-        $logo.removeClass('s1-e2-type');
+        $('.logo').removeClass('s1-e2-type');
     }
 }, {offset: '50%' });
-$s1e3bg.waypoint(function (direction) {
+$('.s1-e3-bg').waypoint(function (direction) {
     if (direction == 'down') {
-        $logo.addClass('s1-e3-type');
+        $('.logo').addClass('s1-e3-type');
     } else {
-        $logo.removeClass('s1-e3-type');
+        $('.logo').removeClass('s1-e3-type');
     }
 }, {offset: '50%' });
-$s1e4bg.waypoint(function (direction) {
+$('.s1-e4-bg').waypoint(function (direction) {
     if (direction == 'down') {
-        $logo.addClass('s1-e4-type');
+        $('.logo').addClass('s1-e4-type');
     } else {
-        $logo.removeClass('s1-e4-type');
+        $('.logo').removeClass('s1-e4-type');
     }
 }, {offset: '50%' });
-$s1e5bg.waypoint(function (direction) {
+$('.s1-e5-bg').waypoint(function (direction) {
     if (direction == 'down') {
-        $logo.addClass('s1-e5-type');
+        $('.logo').addClass('s1-e5-type');
     } else {
-        $logo.removeClass('s1-e5-type');
+        $('.logo').removeClass('s1-e5-type');
     }
 }, {offset: '50%' });
-$s1e6bg.waypoint(function (direction) {
+$('.s1-e6-bg').waypoint(function (direction) {
     if (direction == 'down') {
-        $logo.addClass('s1-e6-type');
+        $('.logo').addClass('s1-e6-type');
     } else {
-        $logo.removeClass('s1-e6-type');
+        $('.logo').removeClass('s1-e6-type');
     }
 }, {offset: '50%' });
-$s1e7bg.waypoint(function (direction) {
+$('.s1-e7-bg').waypoint(function (direction) {
     if (direction == 'down') {
-        $logo.addClass('s1-e7-type');
+        $('.logo').addClass('s1-e7-type');
     } else {
-        $logo.removeClass('s1-e7-type');
+        $('.logo').removeClass('s1-e7-type');
     }
 }, {offset: '50%' });
-$s1e8bg.waypoint(function (direction) {
+$('.s1-e8-bg').waypoint(function (direction) {
     if (direction == 'down') {
-        $logo.addClass('s1-e8-type');
+        $('.logo').addClass('s1-e8-type');
     } else {
-        $logo.removeClass('s1-e8-type');
+        $('.logo').removeClass('s1-e8-type');
     }
 }, {offset: '50%' });
-$s1e9bg.waypoint(function (direction) {
+$('.s1-e9-bg').waypoint(function (direction) {
     if (direction == 'down') {
-        $logo.addClass('s1-e9-type');
+        $('.logo').addClass('s1-e9-type');
     } else {
-        $logo.removeClass('s1-e9-type');
+        $('.logo').removeClass('s1-e9-type');
     }
 }, {offset: '50%' });
-$s1e10bg.waypoint(function (direction) {
+$('.s1-e10-bg').waypoint(function (direction) {
     if (direction == 'down') {
-        $logo.addClass('s1-e10-type');
+        $('.logo').addClass('s1-e10-type');
     } else {
-        $logo.removeClass('s1-e10-type');
+        $('.logo').removeClass('s1-e10-type');
+    }
+}, {offset: '50%' });
+$('.s2-e1-bg').waypoint(function (direction) {
+    if (direction == 'down') {
+        $('.logo').addClass('s2-e1-type');
+    } else {
+        $('.logo').removeClass('s2-e1-type');
+    }
+}, {offset: '50%' });
+$('.s2-e2-bg').waypoint(function (direction) {
+    if (direction == 'down') {
+        $('.logo').addClass('s2-e2-type');
+    } else {
+        $('.logo').removeClass('s2-e2-type');
+    }
+}, {offset: '50%' });
+$('.s2-e3-bg').waypoint(function (direction) {
+    if (direction == 'down') {
+        $('.logo').addClass('s2-e3-type');
+    } else {
+        $('.logo').removeClass('s2-e3-type');
+    }
+}, {offset: '50%' });
+$('.s2-e4-bg').waypoint(function (direction) {
+    if (direction == 'down') {
+        $('.logo').addClass('s2-e4-type');
+    } else {
+        $('.logo').removeClass('s2-e4-type');
+    }
+}, {offset: '50%' });
+$('.s2-e5-bg').waypoint(function (direction) {
+    if (direction == 'down') {
+        $('.logo').addClass('s2-e5-type');
+    } else {
+        $('.logo').removeClass('s2-e5-type');
+    }
+}, {offset: '50%' });
+$('.s2-e6-bg').waypoint(function (direction) {
+    if (direction == 'down') {
+        $('.logo').addClass('s2-e6-type');
+    } else {
+        $('.logo').removeClass('s2-e6-type');
+    }
+}, {offset: '50%' });
+$('.s2-e7-bg').waypoint(function (direction) {
+    if (direction == 'down') {
+        $('.logo').addClass('s2-e7-type');
+    } else {
+        $('.logo').removeClass('s2-e7-type');
+    }
+}, {offset: '50%' });
+$('.s2-e8-bg').waypoint(function (direction) {
+    if (direction == 'down') {
+        $('.logo').addClass('s2-e8-type');
+    } else {
+        $('.logo').removeClass('s2-e8-type');
+    }
+}, {offset: '50%' });
+$('.s2-e9-bg').waypoint(function (direction) {
+    if (direction == 'down') {
+        $('.logo').addClass('s2-e9-type');
+    } else {
+        $('.logo').removeClass('s2-e9-type');
+    }
+}, {offset: '50%' });
+$('.s2-e10-bg').waypoint(function (direction) {
+    if (direction == 'down') {
+        $('.logo').addClass('s2-e10-type');
+    } else {
+        $('.logo').removeClass('s2-e10-type');
     }
 }, {offset: '50%' });
